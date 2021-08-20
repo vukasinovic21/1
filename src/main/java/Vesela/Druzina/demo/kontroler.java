28f2c29f58610c3a88d808785d696b98eab3c925
vukasinovic21 <nikolavukasinovic8@gmail.com>
2021-08-20 02:06:31 +0200

    Update kontroler.java

--- src/main/java/Vesela/Druzina/demo/kontroler.java
+++ src/main/java/Vesela/Druzina/demo/kontroler.java
@@ -1,14 +1,22 @@
 package Vesela.Druzina.demo;
 
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import java.security.Principal;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//import java.util.List;
 
 @RestController
 public class kontroler 
 {
-
+    //DB b = new DB();
     /*@GetMapping("/")
     public String home()
     {
