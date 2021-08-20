0e5bd70ed99c697dc0500f9e6a4e512c87c722ab
vukasinovic21 <nikolavukasinovic8@gmail.com>
2021-08-20 21:43:29 +0200

    SignUp button
    Izmene na html, css i js delu

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -23,6 +23,17 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
+    // Get the modal
+    var modal = document.getElementById('id01');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event)
+    {
+    if (event.target == modal)
+    {
+        modal.style.display = "none";
+    }
+}
     // Shrink the navbar 
     navbarShrink();
 
