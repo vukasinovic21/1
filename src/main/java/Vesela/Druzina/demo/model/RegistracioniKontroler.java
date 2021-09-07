8b716c0d8e88e08ffd607bb5db0b2826daff24de
Dusan Markovic <dusan112233@gmail.com>
2021-09-07 14:05:02 +0200

    register.html

--- src/main/java/Vesela/Druzina/demo/model/RegistracioniKontroler.java
+++ src/main/java/Vesela/Druzina/demo/model/RegistracioniKontroler.java
@@ -10,7 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
-import static com.javadevjournal.ApplicationConstant.REDIRECT;
+//import static com.javadevjournal.ApplicationConstant.REDIRECT;
 
 @Controller
 public class RegistracioniKontroler {
@@ -18,25 +18,33 @@ public class RegistracioniKontroler {
     @Autowired
     private KorisnikService korisnikService;
 
+
+    @GetMapping("/")
+    public String djokica(final Model model){
+        model.addAttribute("korisnikData", new KorisnikData());
+        return "index";
+    }
+
     @GetMapping("/register")
     public String registruj(final Model model){
         model.addAttribute("korisnikData", new KorisnikData());
-        return "account/register";
+        return "nalog/register";
     }
 
     @PostMapping("/register")
     public String registracijaKorisnika(final @Valid  KorisnikData korisnikData, final BindingResult bindingResult, final Model model){
         if(bindingResult.hasErrors()){
             model.addAttribute("registrationForm", korisnikData);
-            return "account/register";
+            return "nalog/register";
         }
         try {
             korisnikService.registrujSe(korisnikData);
         }catch (KorisnikVecPostoji e){
             bindingResult.rejectValue("email", "korisnikData.email","Korisnik sa ovom email adresom vec postoji.");
             model.addAttribute("registrationForm", korisnikData);
-            return "account/register";
+            return "nalog/register";
         }
-        return REDIRECT+"/starter";
+        //return REDIRECT+"/starter";
+        return "/";
     }
 }
