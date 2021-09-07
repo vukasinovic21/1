4b6b292bad13b85a3deef952d8a90019f5e6ff94
vukasinovic21 <nikolavukasinovic8@gmail.com>
2021-09-07 16:12:32 +0200

    sign

--- src/main/java/Vesela/Druzina/demo/kontroler.java
+++ src/main/java/Vesela/Druzina/demo/kontroler.java
@@ -38,6 +38,12 @@ public class Kontroler
         return "Follow @dusan_markovic7";
     }
 
+    @GetMapping("/index.html")
+    public String houm()
+    {
+        return "index";
+    }
+
     @GetMapping("/matrif")
     public String matrif()
     {
